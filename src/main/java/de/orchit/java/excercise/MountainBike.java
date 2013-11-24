2a2bf30253d809e6275b13a49893c209669b6737
pcornelissen <cornelis@pcornelissen.de>
2013-11-24 16:17:04 +0100

    added MountainBike

---
+++ src/main/java/de/orchit/java/excercise/MountainBike.java
@@ -0,0 +1,22 @@
+package de.orchit.java.excercise;
+
+public class MountainBike extends Bike{
+    private int gear;
+    private int seatHeight;
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+
+    public int getSeatHeight() {
+        return seatHeight;
+    }
+
+    public void setSeatHeight(int seatHeight) {
+        this.seatHeight = seatHeight;
+    }
+}
